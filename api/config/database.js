26aebff915e71601a280cd425d7f68e7a9f07ba0
nd4pa <antoine.blondeau@isen-bretagne.fr>
2020-03-24 20:56:25 +0100

    Fix db config for clever

--- api/config/database.js
+++ api/config/database.js
@@ -1,4 +1,12 @@
 const Sequelize = require('sequelize');
+require('process');
+
+var port = process.env.POSTGRESQL_ADDON_DB || "b3t"
+var port = process.env.POSTGRESQL_ADDON_HOST || '127.0.0.1'
+var port = process.env.POSTGRESQL_ADDON_PASSWORD || "b3t"
+var port = process.env.POSTGRESQL_ADDON_PORT || 5432
+var port = process.env.POSTGRESQL_ADDON_USER || "b3t"
+var port = process.env.POSTGRESQL_ADDON_VERSION || "11"
 
 const db = new Sequelize('b3t', 'b3t', 'b3t', {
     host: '127.0.0.1',
