77ea210ebf01c70f6e3a5a87933ae4ae8d578a8c
nd4pa <antoine.blondeau@isen-bretagne.fr>
2020-03-24 20:25:14 +0100

    add site to api

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 const userController = require('./Controllers/UserController');
+const path = require('path');
 const Express = require('express')
 const app = new Express()
 
@@ -7,6 +8,23 @@ const sequelize = require('./config/database');
 app.get('/user', userController.list)
 app.get('/user/:id', userController.show)
 
+app.use(Express.static(__dirname + '/site'));
+
+app.get('/',function(req,res) {
+  res.sendFile(path.join(__dirname+'/site/accueil.html'));
+});
+
+app.get('/login',function(req,res) {
+  res.sendFile(path.join(__dirname+'/site/login.html'));
+});
+
+app.get('/parcours',function(req,res) {
+  res.sendFile(path.join(__dirname+'/site/parcours.html'));
+});
+
+app.get('/activites',function(req,res) {
+  res.sendFile(path.join(__dirname+'/site/activites.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
