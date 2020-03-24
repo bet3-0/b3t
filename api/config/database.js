56207e6fe8682199820a76ab30dcc7c32388c42b
nd4pa <antoine.blondeau@isen-bretagne.fr>
2020-03-24 21:12:18 +0100

    Fix db and port for clever

---
+++ api/config/database.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize');
+require('process');
+
+var db = process.env.POSTGRESQL_ADDON_DB || "b3t"
+var host = process.env.POSTGRESQL_ADDON_HOST || '127.0.0.1'
+var password = process.env.POSTGRESQL_ADDON_PASSWORD || "b3t"
+var port = process.env.POSTGRESQL_ADDON_PORT || 5432
+var user = process.env.POSTGRESQL_ADDON_USER || "b3t"
+var version = process.env.POSTGRESQL_ADDON_VERSION || "11"
+
+const db = new Sequelize(db, user, password, {
+    host: host,
+    dialect: 'postgres'
+});
+
+db.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+db.sync()
+    .then()
+    .catch((err) => {
+        console.log(err)
+    });
+
+module.exports = db
