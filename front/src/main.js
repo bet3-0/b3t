78bd68094b3cf38faace29068cee3fbb763d1d5d
vfa <victorrfau@gmail.com>
2020-03-29 21:08:15 +0100

    ajout de la route de login

--- front/src/main.js
+++ front/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import HomePageComponent from './components/HomePageComponent'
 import VueRouter from 'vue-router'
+import HomePageComponent from './components/HomePageComponent'
+import LoginComponent from "./components/LoginComponent"
 import App from './App.vue'
 
 
@@ -9,10 +10,16 @@ Vue.config.productionTip = false
 
 const router = new VueRouter({
   mode: 'history',
-  routes: [{
-    path: '/',
-    component: HomePageComponent
-  }]
+  routes: [
+      {
+        path: '/',
+        component: HomePageComponent
+      },
+      {
+        path: '/login',
+        component: LoginComponent
+      },
+  ]
 })
 
 new Vue({
