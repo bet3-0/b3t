ca30cd90bc251325de2594c3b6552207a4bee09a
victor fau <v.fau@arep.co.com>
2020-04-02 15:32:59 +0200

    ajout du lien sur le clique de début d'activité

--- front/src/components/includes/ActivityModal.vue
+++ front/src/components/includes/ActivityModal.vue
@@ -1,39 +1,46 @@
 <template>
-  <div>
-    <b-modal
-      class="modal-backdrop"
-      id="activityModal"
-      :title="activity.nom"
-      hide-backdrop
-    >
-      <div class="modal-body">
-        <p>Description: {{ activity.description }}</p>
-        <p>Durée: {{ activity.duree }}</p>
-        <p>Matériel: {{ activity.materiel }}</p>
-      </div>
-      <template v-slot:modal-footer="{ ok, cancel }">
-        <b-button variant="secondary" @click="cancel()">
-          Fermer
-        </b-button>
-        <form :action="'activite/' + activity.parcours + '/' + activity.id">
-          <b-button variant="success" @click="ok()" type="submit">
-            Démarrer l'activité
-          </b-button>
-        </form>
-      </template>
-    </b-modal>
-  </div>
+    <div>
+        <b-modal
+                class="modal-backdrop"
+                id="activityModal"
+                :title="activity.nom"
+                hide-backdrop
+        >
+            <div class="modal-body">
+                <p>Description: {{ activity.description }}</p>
+                <p>Durée: {{ activity.duree }}</p>
+                <p>Matériel: {{ activity.materiel }}</p>
+            </div>
+            <template v-slot:modal-footer="{ ok, cancel }">
+                <b-button variant="secondary" @click="cancel()">
+                    Fermer
+                </b-button>
+                <b-button variant="success" @click.prevent.capture="go(activity.id)">
+                    Démarrer l'activité
+                </b-button>
+            </template>
+        </b-modal>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "ActivityModal",
-  props: ["activity"]
-};
+  import Vue from 'vue'
+  import VueRouter from 'vue-router'
+  Vue.use(VueRouter)
+    export default {
+        name: "ActivityModal",
+        props: ["activity"],
+      methods: {
+          go(activityId) {
+            console.log(activityId)
+            this.$router.push('/activitees/' + activityId)
+          }
+      }
+    };
 </script>
 
 <style scoped>
-.modal-backdrop {
-  opacity: 0.5 !important;
-}
+    .modal-backdrop {
+        opacity: 0.5 !important;
+    }
 </style>
