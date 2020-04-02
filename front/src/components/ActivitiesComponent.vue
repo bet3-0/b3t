96f6c1687bec5e48d182b9677aef019dba722d9f
vfa <victorrfau@gmail.com>
2020-04-02 09:54:33 +0100

    avancement

--- front/src/components/ActivitiesComponent.vue
+++ front/src/components/ActivitiesComponent.vue
@@ -1,47 +1,34 @@
 <template>
   <div class="container">
-    <div id="wrapper" class="toggled">
+    <div id="wrapper" class="toggled choices-container">
       <!-- Sidebar: TODO -->
       <div id="sidebar-wrapper">
-        <ul class="sidebar-nav">
-          <li class="sidebar-brand">
-            <a href="#">Parcours {{ getParcoursName(idParcours) }} </a>
-          </li>
-          <li><a href="#">Classer par durée</a></li>
-          <li><a href="#">Classer par difficulté</a></li>
-          <li><a href="#">Activités rouges</a></li>
+        <ul class="sidebar-nav choices">
+          <li class="choice" >Parcours {{ getParcoursName(idParcours) }}</li>
+          <li class="choice" >Classer par durée</li>
+          <li class="choice" >Classer par difficulté</li>
+          <li class="choice" >Activités rouges</li>
         </ul>
       </div>
       <!-- /#sidebar-wrapper -->
     </div>
     <!-- /#wrapper -->
     <div class="container">
-      <ul id="activities" class="list-group" style="margin-top: 10%">
-        <li
-          class="list-group-item"
-          v-for="activity in activities"
-          :key="activity.id"
-          :style="'background:' + getParcoursColor(activity.idParcours)"
-        >
+      <ul id="activities" class="list-group">
+        <li class="list-group-item" v-for="activity in orderActivities" :key="activity.id">
           <a href="#" v-b-modal="'activityModal'" @click="sendInfo(activity)">
-            <svg
-              class="bi bi-play-fill"
-              width="1em"
-              height="1em"
-              viewBox="0 0 16 16"
-              fill="currentColor"
-              style="display: inline"
-            >
+            <svg class="bi bi-play-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" style="display: inline">
               <path
                 d="M11.596 8.697l-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 010 1.393z"
               />
             </svg>
-            <p style="display: inline">{{ activity.nom }}</p>
+            <p style="display: inline-block">{{ activity.nom }}</p>
+            <p style="display: inline-block; margin-left: 3em" >{{getParcoursName(activity.idParcours)}}</p>
             <p
               style="display: inline-block; position: absolute; right: 0; margin-right: 5px"
               class="justify-content-end"
             >
-              Durée : {{ activity.duree }}
+              Durée : {{ activity.duree }} min
             </p>
           </a>
         </li>
@@ -57,7 +44,10 @@
 import ActivityModal from "./includes/ActivityModal";
 import Vue from "vue";
 import BootstrapVue from "bootstrap-vue";
+import VueRouter from "vue-router";
+
 Vue.use(BootstrapVue);
+Vue.use(VueRouter);
 
 export default {
   name: "ActivitiesComponent",
@@ -67,39 +57,25 @@ export default {
     return {
       currentActivity: {},
       activities: [
-        {
-          id: 0,
-          idParcours: 0,
-          nom: "Learn JavaScript",
-          description: "c'est bien",
-          duree: "5h",
-          materiel: "plein"
-        },
-        { id: 1, idParcours: 0, nom: "Learn Vue", duree: "5h" },
-        { id: 2, idParcours: 1, nom: "Build something awesome", duree: "5h" }
-      ]
+        {id: 0, idParcours: 0, nom: "Learn JavaScript", description: "c'est bien", duree: 22, materiel: "plein"},
+        { id: 1, idParcours: 0, nom: "Learn Vue", duree: 25 },
+        { id: 2, idParcours: 1, nom: "Build something awesome", duree: 20 }
+      ],
+      displayActivities: []
     };
+  },
+  mounted(){
+    this.displayActivities = this.activities
   },
   methods: {
     sendInfo(activity) {
       this.currentActivity = activity;
     },
 
-    // Mettre ça dans un dossier commun:
-    getParcoursColor(idParcours) {
-      switch (idParcours) {
-        case 0:
-          return "#ff8300";
-        case 1:
-          return "#d03f15";
-        case 2:
-          return "#6e74aa";
-        case 3:
-          return "#0072540";
-        default:
-          return "#003a5d";
-      }
+    orderActivities: function () {
+      return _.orderBy(this.activities, 'duree')
     },
+
     getParcoursName(idParcours) {
       switch (idParcours) {
         case 0:
@@ -118,4 +94,30 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+  .choices-container{
+    margin-top: 3em;
+  }
+
+  .choices{
+    display: flex;
+    justify-content: space-evenly;
+  }
+
+  #activities p{
+    margin: 0;
+  }
+
+  #activities p:first-of-type{
+    width: 45%;
+  }
+
+  .choices li{
+    list-style-type: none;
+  }
+
+  .choice{
+    border: solid;
+    cursor: pointer;
+  }
+</style>
