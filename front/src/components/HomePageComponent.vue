54ea827b24b4a1b61db6a85806b74cf752fb0e39
victorfau <33352460+victorfau@users.noreply.github.com>
2020-04-04 15:26:13 +0200

    Revert "Feature/wash hands"

--- front/src/components/HomePageComponent.vue
+++ front/src/components/HomePageComponent.vue
@@ -1,26 +1,13 @@
 <template>
-  <div class="container">
-    <div
-      class="embed-responsive embed-responsive-16by9"
-      style="margin-top: 10%"
-    >
-      <iframe
-        class="embed-responsive-item"
-        width="560"
-        height="315"
-        src="https://www.youtube.com/embed/N4c0zGP1yAg"
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen
-      ></iframe>
-    </div>
-  </div>
+    <h1>home page</h1>
 </template>
 
 <script>
-export default {
-  name: "HomePageComponent"
-};
+    export default {
+        name: "HomePageComponent"
+    }
 </script>
 
-<style scoped></style>
+<style scoped>
+
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,53 +1,28 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import HomePageComponent from "./components/HomePageComponent";
-import LoginComponent from "./components/LoginComponent";
-import ActivitiesComponent from "./components/ActivitiesComponent";
-import ActivityComponent from "./components/ActivityComponent";
-import App from "./App.vue";
-import "@/assets/css/main.css";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomePageComponent from './components/HomePageComponent'
+import LoginComponent from "./components/LoginComponent"
+import App from './App.vue'
 
-Vue.use(VueRouter);
-Vue.config.productionTip = false;
 
-Vue.filter('difficult', function (value) {
-  switch (value) {
-    case 0:
-      return "Très facile";
-    case 1:
-      return "facile";
-    case 2:
-      return "Moyen";
-    case 3:
-      return "Difficile";
-    default:
-      return "Moyen"; // où mettre la halte ?
-  }
-})
+Vue.use(VueRouter)
+Vue.config.productionTip = false
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   routes: [
-    {
-      path: "/",
-      component: HomePageComponent
-    },
-    {
-      path: "/login",
-      component: LoginComponent
-    },
-    {
-      path: "/activitees",
-      component: ActivitiesComponent
-    },
-    {
-      path: "/activity/:idActivity",
-      component: ActivityComponent
-    }
+      {
+        path: '/',
+        component: HomePageComponent
+      },
+      {
+        path: '/login',
+        component: LoginComponent
+      },
   ]
-});
+})
 
 new Vue({
   render: h => h(App),
   router
-}).$mount("#app");
+}).$mount('#app')
