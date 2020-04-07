7d0944b086652e2ffffb3c173ef6bdd27917d51d
guiyom <guiyom@PC2>
2020-04-07 14:52:44 +0200

    add footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,14 +5,17 @@
       <ItineraryProgressBar />
     </div>
     <router-view></router-view>
+    <FooterComponent />
   </div>
 </template>
 
 <script>
 import NavComponent from "./components/includes/NavComponent";
 import ItineraryProgressBar from "./components/includes/ItineraryProgressBar";
+import FooterComponent from "./components/includes/FooterComponent";
 
 export default {
-  components: { NavComponent, ItineraryProgressBar }
+  components: { NavComponent, ItineraryProgressBar, FooterComponent }
 };
 </script>
+
