f4fc2daef21105418f797103d2954b05baaa4aa9
guiyom <guiyom@PC2>
2020-04-07 03:23:07 +0200

    add progression bar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
-    <NavComponent/>
+    <div>
+      <NavComponent />
+      <ItineraryProgressBar :progress="getProgression()" :itineraryId="getItinerary()" />
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-
 import NavComponent from "./components/includes/NavComponent";
+import ItineraryProgressBar from "./components/includes/ItineraryProgressBar";
 
 export default {
-  components: {NavComponent}
-}
+  components: { NavComponent, ItineraryProgressBar },
+  methods: {
+    // Use local storage for more reactivity (avoid call to API).
+    getProgression: () => {
+      return localStorage.getItem("progression") || 0;
+    },
+    getItinerary: () => {
+      return localStorage.getItem("itinerary") || 5;
+    },
+  },
+};
 </script>
-
---
+++ front/src/components/includes/ItineraryProgressBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="progress">
+    <div
+      class="progress-bar progress-bar-animated"
+      role="progressbar"
+      v-bind:style="{ width: progress + '%', background: getItineraryColor(itineraryId) }"
+      aria-valuenow="25"
+      aria-valuemin="0"
+      aria-valuemax="100"
+    >
+      {{ progress }}%
+    </div>
+  </div>
+</template>
+<script>
+import ItineraryHelpers from './../../service/itineraryHelpers';
+
+export default {
+  name: "ItineraryProgressBar",
+  props: { progress: { default: 0 }, itineraryId: { default: 5 } },
+  data() {
+    return {
+      itineraryColor: this.getItineraryColor(this.itineraryId),
+    };
+  },
+  methods: {
+    getItineraryColor: ItineraryHelpers.getItineraryColor
+  }
+};
+</script>
+<style scoped></style>
