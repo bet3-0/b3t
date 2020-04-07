75b07451963c83f23091a7bdd81210af8db6dbc7
Etienne Villedieu <EtienneV@users.noreply.github.com>
2020-04-07 15:34:59 +0200

    Get Qcm responses

--- front/src/components/includes/activityComponents/Qcm.vue
+++ front/src/components/includes/activityComponents/Qcm.vue
@@ -4,23 +4,28 @@
       <div class="col-12">
         <div class="form-group" v-for="(question, indexQ) in questions" :key="indexQ">
           <div class="form-label">
-            <b>Question {{indexQ + 1}} :</b> {{question.question}}
+            <b>Question {{indexQ + 1}} :</b>
+            {{question.question}}
           </div>
           <div class="form-check" v-for="(proposition, indexP) in question.reponses" :key="indexP">
-            <input class="form-check-input" type="radio" :name="'q' + indexQ" :id="'q' + indexQ + 'r' + indexP" value="option1" />
+            <input
+              class="form-check-input"
+              type="radio"
+              :name="'q' + indexQ"
+              :id="'q' + indexQ + 'r' + indexP"
+              v-bind:value="proposition"
+              v-model="question.value"
+            />
             <label class="form-check-label" :for="'q' + indexQ + 'r' + indexP">{{proposition}}</label>
           </div>
         </div>
       </div>
 
+      
 
       <br />
-      
-      <button
-        class="btn btn-primary"
-        type="button"
-        v-on:click="submitText()"
-      >Envoyer mon résultat</button>
+
+      <button class="btn btn-primary" type="button" v-on:click="submitQcm()">Envoyer mes réponses</button>
     </div>
   </div>
 </template>
@@ -31,17 +36,23 @@ export default {
   props: ["activityId", "entryId", "changeEntryState", "questions"],
   data() {
     return {
-      text: ""
+      text: "",
+      q2: 'init'
     };
   },
   created() {
+    for(var i = 0; i < this.questions.length; i++) {
+      this.questions[i].value = undefined;
+    }
+
     console.log(this.questions);
+    console.log(this.q2);
   },
   methods: {
     /* Submits the text to the server */
     // TODO
-    submitText() {
-      console.log("Not Implemented");
+    submitQcm() {
+      console.log(this.questions);
       this.changeEntryState(this.entryId, "finished");
       return {};
     }
