359d78221120229cb9c22fd1f3399801f4fb5e27
Guillaume Égée <46320048+guiyom-e@users.noreply.github.com>
2020-04-07 17:57:57 +0200

    Feat/footer header  + login (#54)
    * front with login
    
    * add main.js uncommited before
    
    * add footer
    
    * navbar header + login deconnection
    
    * minor improvements + parcours redirects to activitess/ when chosen
    
    Co-authored-by: guiyom <guiyom@PC2>

--- front/src/components/ParcoursChoiceComponent.vue
+++ front/src/components/ParcoursChoiceComponent.vue
@@ -1,49 +1,68 @@
 <template>
-    <div class="container">
-        <h1>Choisi ton parcours</h1>
-        <div class="row">
-            <div class="col-md-3">
-                <img @click="selectChoice('bosse et bobos')" src="@/assets/img/bosseEtBobo.png" alt="">
-            </div>
-            <div class="col-md-3">
-                <img @click="selectChoice('robinson')" src="@/assets/img/robinson.png" alt="">
-            </div>
-            <div class="col-md-3">
-                <img @click="selectChoice('cesarts')" src="@/assets/img/cesArt.png" alt="">
-            </div>
-            <div class="col-md-3">
-                <img @click="selectChoice('trois étoiles')" src="@/assets/img/troisEtoiles.png" alt="">
-            </div>
-        </div>
+  <div class="container">
+    <h1>Choisi ton parcours</h1>
+    <div class="row">
+      <div class="col-md-3">
+        <img
+          @click="selectChoice('bosse et bobos')"
+          src="@/assets/img/bosseEtBobo.png"
+          alt=""
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          @click="selectChoice('robinson')"
+          src="@/assets/img/robinson.png"
+          alt=""
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          @click="selectChoice('cesarts')"
+          src="@/assets/img/cesArt.png"
+          alt=""
+        />
+      </div>
+      <div class="col-md-3">
+        <img
+          @click="selectChoice('trois étoiles')"
+          src="@/assets/img/troisEtoiles.png"
+          alt=""
+        />
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import VueRouter from "vue-router";
 
-    import Vue from 'vue'
-    import VueRouter from "vue-router";
+Vue.use(VueRouter);
 
-    Vue.use(VueRouter)
-
-    export default {
-        name: "ParcoursChoiceComponent",
-        methods: {
-            selectChoice(selected){
-                console.log(selected)
-                this.$router.push('/activitees')
-            },
-        }
+export default {
+  name: "ParcoursChoiceComponent",
+  created() {
+    // if a parcours has been chosen, redirect to /activitees
+    if (localStorage.getItem("parcours")) {
+      this.$router.push("/activitees");
     }
+  },
+  methods: {
+    selectChoice(selected) {
+      console.log(selected);
+      this.$router.push("/activitees");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .container{
-        text-align: center;
-    }
-    .col-md-3 img{
-        max-width: 100%;
-        cursor: pointer;
-    }
-
-
+.container {
+  text-align: center;
+}
+.col-md-3 img {
+  max-width: 100%;
+  cursor: pointer;
+}
 </style>
