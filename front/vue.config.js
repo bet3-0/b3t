e4521403290f89b29bbc1d96507cbfd7b074bad9
victor <victorrfau@gmail.com>
2020-04-12 17:11:39 +0200

    en attente du lien api pour etre tester en profondeur

---
+++ front/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    chainWebpack: config => {
+        config.module
+            .rule('html')
+            .test(/\.html$/)
+            .use('html-loader')
+            .loader('html-loader')
+            .end()
+    }
+}
