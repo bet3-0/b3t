ed2614618b4cf70e051ae7b760eaa587932ff083
nd4pa <antoine.blondeau@isen-bretagne.fr>
2020-04-18 00:50:42 +0200

    Rework file workflow in front

--- front/src/service/progression.service.js
+++ front/src/service/progression.service.js
@@ -29,40 +29,6 @@ export default class ProgressionService {
     });
   }
 
-  static async pushFile(data) {
-    console.log("Pushing file...");
-    let response;
-    try {
-      response = await fetch(API_URL + "file", {
-        method: "POST",
-        headers: Object.assign(authHeader(), {
-          "Content-Type": "application/octet-stream",
-        }),
-        body: data,
-      });
-    } catch (error) {
-      console.log("Error while sending file: fetch error!");
-      console.error(error);
-      return undefined;
-    }
-    if (!response.ok) {
-      console.log("Error while sending file: response error!");
-      console.warn(response);
-      return undefined;
-    }
-
-    let jsonResponse;
-    try {
-      jsonResponse = await response.json();
-    } catch (error) {
-      console.log("Error while sending file: response data error!");
-      console.warn(response);
-      return undefined;
-    }
-    console.log("File sent successfully!");
-    return jsonResponse.url;
-  }
-
   static async getProgressions() {
     console.log("Fetching progressions...");
     try {
