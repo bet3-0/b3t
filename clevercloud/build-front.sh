a5e81fb2871899e8829a03d621c4a20d0725c40c
Julien Durillon <julien.durillon@clever-cloud.com>
2020-04-18 16:03:08 +0200

    fix build script.

--- clevercloud/build-front.sh
+++ clevercloud/build-front.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -l
 
-pushd front
-export NODE_ENV=production
-npm install
-npm run build
+set -x
 
+pushd front
+
+NODE_ENV=production npm install
+NODE_ENV=production npm run build
+
+popd
