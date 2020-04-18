188e56f3b23d0c13d3f52385032040306b9b98de
nd4pa <antoine.blondeau@isen-bretagne.fr>
2020-04-18 13:13:16 +0200

    Fix timestamp

--- progression.go
+++ progression.go
@@ -56,10 +56,6 @@ type Progression struct {
 	CodeAdherent string
 }
 
-func makeTimestamp() int64 {
-	return time.Now().UnixNano() / int64(time.Millisecond)
-}
-
 func CreateProgression(c *gin.Context) {
 	var progression Progression
 
@@ -76,7 +72,7 @@ func CreateProgression(c *gin.Context) {
 	id, _ = uuid.NewRandom()
 	progression.ID = id.String()
 
-	progression.StartedAt = makeTimestamp()
+	progression.StartedAt = time.Now().Unix()
 
 	user := c.Request.Context().Value("user").(User)
 
@@ -171,11 +167,11 @@ func UpdateProgression(c *gin.Context) {
 	progression.CodeAdherent = user.CodeAdherent
 
 	if progression.State == state("FINISHED") {
-		progression.FinishedAt = makeTimestamp()
+		progression.FinishedAt = time.Now().Unix()
 	}
 
 	if progression.State == state("VALIDATED") || progression.State == state("REFUSED") {
-		progression.ReviewdAt = makeTimestamp()
+		progression.ReviewdAt = time.Now().Unix()
 	}
 
 	err = db.Save(&progression).Error
