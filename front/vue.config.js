ed54e1a705679c37cfab6b967f046fdee95d15c7
nd4pa <antoine.blondeau@isen-bretagne.fr>
2020-04-18 11:01:58 +0200

    Fix file umport in webpack

--- front/vue.config.js
+++ front/vue.config.js
@@ -5,6 +5,11 @@ module.exports = {
             .test(/\.html$/)
             .use('html-loader')
             .loader('html-loader')
+        config.module
+            .rule('files')
+            .test(/\.(png|jpe?g|gif|m4a)$/i)
+            .use('file-loader')
+            .loader('file-loader')
     },
     devServer: {
         proxy: 'http://bet3-0.sgdf.fr/api',
