4ba7313b24c1ed4e5a54a34545dacaf5d05460bc
guiyom-e <guillaume.egee@outlook.com>
2020-04-19 23:19:57 +0200

    clean unused and confusing updateProgression in store.progression

--- front/src/store/progression.module.js
+++ front/src/store/progression.module.js
@@ -7,22 +7,14 @@ export const progression = {
   },
   actions: {
     setProgression({ commit }, prog) {
-      console.log("New progression: " + prog);
       commit("set", parseInt(prog));
       localStorage.setItem("progression", this.state.progression.progression);
-    },
-    updateProgression({ commit }, prog) {
-      console.log("Progression updated: +" + prog);
-      commit("update", parseInt(prog));
-      localStorage.setItem("progression", this.state.progression.progression);
+      console.log("Global progression: " + this.state.progression.progression + ' %');
     },
   },
   mutations: {
     set(state, prog) {
       state.progression = Math.min(prog / 3, 100);
-    },
-    update(state, prog) {
-      state.progression = Math.min(state.progression + prog / 3, 100);
     },
   },
 };
