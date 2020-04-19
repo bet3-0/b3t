de1bea95d5f82f0feefcdc076dcb93a84d9f0f02
Thomas Gall <thomas.gall@actimage.com>
2020-04-19 11:46:46 +0200

    Change home video

--- front/src/components/HomePageComponent.vue
+++ front/src/components/HomePageComponent.vue
@@ -4,11 +4,7 @@
       class="embed-responsive embed-responsive-16by9"
       style="margin-top: 10%"
     >
-      <iframe width="560" height="315" 
-      src="https://www.youtube.com/embed/pCv2FMxvfvc" 
-      frameborder="0" 
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" 
-      allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/DqSGv0IFlGk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
   </div>
 </template>
