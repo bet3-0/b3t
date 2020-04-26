453d379dd7012694fca397a675dd1b2f47504aaf
guiyom-e <guillaume.egee@outlook.com>
2020-04-26 18:34:08 +0200

    add la halte and célébration to CongratsModal

--- front/src/components/includes/CongratsModal.vue
+++ front/src/components/includes/CongratsModal.vue
@@ -10,7 +10,8 @@
         Félicitations ! Tes activités ont été validées et tu viens
         donc de terminer le parcours <b>{{ getParcoursName(idParcours) }}</b> !
         <br><br>Tu es maintenant un éclaireur ou une éclaireuse de Tribu aguerri(e) :
-        à toi maintenant d'utiliser et transmettre tout ce que tu as appris dans ta tribu !
+        à toi maintenant d'utiliser et transmettre tout ce que tu as appris dans ta tribu !<br><br>
+        Enfin, n'oublie pas de participer à <a href="/halte">la Halte</a> si tu ne l'as pas encore fait !
       </div>
       <img :src="require(`@/assets/img/${getParcoursImageName(idParcours)}`)"
            style="margin-left: auto; margin-right: auto; display: block; width: 70%;"/>
@@ -23,11 +24,15 @@
       <b-button @click="cancel()" variant="secondary">
         Retour
       </b-button>
-      <b-button
-        @click.prevent.capture="$router.push('/activitees')"
-        variant="success">
-        Voir toutes les activités
-      </b-button>
+      <a
+        href="https://www.youtube.com/watch?v=7v-rEFvKf2k&list=PLR5Q4mdTpY45X31Lb98Fw0Ljh4CrOP_Hc&index=4&t=0s"
+        rel="noopener noreferrer"
+        target="_blank">
+        <b-button
+          variant="success">
+          Voir la célébration de clôture
+        </b-button>
+      </a>
     </template>
   </b-modal>
 </template>
